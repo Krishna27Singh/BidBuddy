bfa8e44683a2bbb85ebddb2aa408a7bfd6972370
Krishna27Singh <krishnasingh97857@gmail.com>
2025-06-24 12:37:49 +0530

    new

--- server/server.js
+++ server/server.js
@@ -11,26 +11,14 @@ connectDB();
 // Middleware
 app.use(express.json());
 
-const allowedOrigins = ['http://localhost:8080', 'https://www.omnidim.io'];
+app.use(cors({
+  origin: '*', // Allow all origins
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Allowed HTTP methods
+  allowedHeaders: ['Content-Type', 'Authorization'], // Allowed headers
+  credentials: true, // Enable credentials
+}));
 
-app.use((req, res, next) => {
-  const origin = req.headers.origin;
-  console.log(origin);
-  if (allowedOrigins.includes(origin)) {
-    res.setHeader('Access-Control-Allow-Origin', origin); // Dynamically set the allowed origin
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    res.setHeader('Access-Control-Allow-Credentials', 'true');
-  }
-  
-  // Handle OPTIONS preflight requests
-  if (req.method === 'OPTIONS') {
-    return res.status(200).end();
-  }
-
-  next();
-});
-
+app.options('*', cors());
 
 
 app.use((req, res, next) => {
